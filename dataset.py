3ca826ecb34fac97cdbeaafac7c9449e3925858a
ilim <ilim0t14@gmail.com>
2019-11-15 17:17:43 +0900

    Add dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import csv
+from typing import List
+
 from torch.utils.data import Dataset
 
 
@@ -16,3 +19,29 @@ class SampleDataset(Dataset):
 
     def __repr__(self) -> str:
         raise NotImplementedError
+
+
+class SimplifiedDataset(Dataset):
+    def __init__(self, data_file: str, transforms=None) -> None:
+        self.data_file = data_file
+        self.transforms = transforms
+
+        self.data = self.load_data(data_file)
+
+    def load_data(self, data_file: str) -> List[List[str]]:
+        with open(data_file) as f:
+            data = list(csv.reader(f))
+        return data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> List[str]:
+        return self.data[index]
+
+
+if __name__ == "__main__":
+    dataset = SimplifiedDataset("./dataset.csv")
+    print(len(dataset))
+    print(f"dataset[0]: {dataset[0]}")
+    print(f"dataset[-1]: {dataset[-1]}")
