76991369caac154942bb09a0b6dfa248ff8f9752
muhiyuyuyu <8619254@ed.tus.ac.jp>
2019-11-23 11:32:13 +0900

    add combobox

--- window.py
+++ window.py
@@ -1,21 +1,34 @@
 import sys
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-root = tk.Tk()
-root.title(u"select kigo")
-width = 800
-height = 600
-root.geometry("{0}x{1}".format(width,height))
+def button_clicked(event):
+    val = v1.get()
+    lv = tk.Label(text=val)
+    lv.pack_forget()
+    lv.pack(side=tk.BOTTOM)
 
-kigo = "桜"
+def cb_selected(event):
+    print('v1 = %s' % v1.get())
 
-def SelectKigo(x):
-    static = tk.Label(text=kigo)
-    static.pack(side=tk.BOTTOM)
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title(u"select kigo")
+    width = 800
+    height = 600
+    root.geometry("{0}x{1}".format(width,height))
 
-Button = tk.Button(text=kigo, width=10)
-Button.bind("<Button-1>",SelectKigo)
+    v1 = StringVar()
+    cb = ttk.Combobox(textvariable=v1)
+    cb.bind('<<ComboboxSelected>>', cb_selected)
 
-Button.pack()
+    cb['values']=("桜","海","紅葉","雪")
+    cb.set("桜")
+    cb.pack()
 
-root.mainloop()
+    Button = tk.Button(text="OK", width=10)
+    Button.bind("<Button-1>",button_clicked)
+    Button.pack()
+
+    root.mainloop()
